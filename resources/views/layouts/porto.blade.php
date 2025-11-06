ead6810fa40d2872417e9fd3644b9645a8471a0b
ENES EKİNCİ <67474580+enesekinci@users.noreply.github.com>
2025-11-06 12:15:30 +0300

    theme layout file

---
+++ resources/views/layouts/porto.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('components.porto.head', [
+    'title' => $title ?? 'Porto - Bootstrap eCommerce Template',
+    'keywords' => $keywords ?? 'HTML5 Template',
+    'description' => $description ?? 'Porto - Bootstrap eCommerce Template',
+    'demoCss' => $demoCss ?? 'demo1',
+])
+
+<body
+@if(isset($bodyClass) && $bodyClass)
+ class="{{ $bodyClass }}"
+@endif
+@if(isset($bodyAttributes) && $bodyAttributes)
+ {{ $bodyAttributes }}
+@endif>
+    @include('components.porto.loading-overlay')
+    <div class="page-wrapper">
+        @hasSection('top-notice')
+            @yield('top-notice')
+        @endif
+
+        @hasSection('header')
+            @yield('header')
+        @else
+            @include('components.porto.header')
+        @endif
+
+        <main class="main{{ $mainClass ? ' ' . $mainClass : '' }}">
+            @yield('content')
+        </main>
+
+        @hasSection('footer')
+            @yield('footer')
+        @else
+            @include('components.porto.footer')
+        @endif
+    </div>
+
+    @include('components.porto.mobile-menu-overlay')
+    @include('components.porto.mobile-menu')
+    @hasSection('sticky-navbar')
+        @yield('sticky-navbar')
+    @else
+        @include('components.porto.sticky-navbar')
+    @endif
+    @include('components.porto.newsletter-popup')
+    @include('components.porto.scroll-top')
+
+    @include('components.porto.scripts')
+</body>
+
+</html>
+
