e7536c22ebe5296a20e8eaa6fdba46bdf8d62254
ENES EKİNCİ <67474580+enesekinci@users.noreply.github.com>
2025-11-07 00:21:24 +0300

    components

--- resources/views/components/porto/top-notice-demo11.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="top-notice mb-0 pl-0">
-    <div class="container justify-content-center">
-        <div class="header-left">
-            <h5 class="top-notice mb-0 pl-0">Get 10% OFF at the Porto Fashion Selection - <a href="/porto/demo11-shop.html">Shop Now!</a></h5>
-        </div>
-    </div>
-</div>
-
--- resources/views/components/porto/top-notice-demo16.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="top-notice mb-0 text-white">
-
-                            <h5 class="mb-0">Get 10% OFF at the Porto Fashion Selection -
-                                <a href="/porto/demo16-shop.html" class="text-white">Shop Now!</a>
-                            </h5>
-                        
-</div>
